23defed205e2ce272a7260d17024c953bd0b5550
10994015 <a0938599191@gmail.com>
2022-10-29 18:53:23 +0800

    play ball

--- resources/views/livewire/airplane.blade.php
+++ resources/views/livewire/airplane.blade.php
@@ -1,7 +1,25 @@
-<div>
-    @if(!Auth::check())
-      <h1>Not login!!</h1>
-    @else
-        <h1>Airplane!!</h1>
-    @endif
+<div id="airplane">
+    <div class="header">
+      <div class="left">
+        <div class="userBox">
+          <img src="/images/airplane/w644.jpg">
+          <div class="userNameBox">{{Auth::user()->name}}</div>
+        </div>
+        <div class="moneyBox">
+          <img src="/images/airplane/money-icon.png">
+          <div class="dollerBox">$ {{Auth::user()->money}}</div>
+        </div>
+      </div>
+      <div class="right">
+        <div class="fpsBox">90ms</div>
+        <i class="fas fa-bars" id="bar"></i>
+      </div>
+  </div>
+  <div class="content">
+    <div class="playBox">
+      
+    </div>
+    <div class="betBox"></div>
+    <div class="betList"></div>
+  </div>
 </div>
--- resources/views/livewire/layouts/game.blade.php
+++ resources/views/livewire/layouts/game.blade.php
@@ -7,6 +7,7 @@
     <title>DYM</title>
     <link rel="stylesheet" href="/scss/app.css">
     <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
     @livewireStyles
 </head>
 <body>
