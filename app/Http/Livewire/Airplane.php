2530e7fa4dfef715739556a845a99551195d60e6
10994015 <a0938599191@gmail.com>
2022-12-02 14:57:27 +0800

    1202

--- app/Http/Livewire/Airplane.php
+++ app/Http/Livewire/Airplane.php
@@ -7,6 +7,7 @@ use App\Models\Betlist;
 use App\Models\GameInfos;
 use App\Models\GameStatu;
 use App\Models\RiskBet;
+use App\Models\Answer as ModelsAnswer;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
@@ -22,7 +23,14 @@ class Airplane extends Component
     public $odds;
     public $statu;
     protected $listeners  = ['sendTime'=>'sendTime', 'noneLoad'=>'noneLoad', 'chkBet'=>'chkBet', 'calcMoney'=>'calcMoney', 'updateMyMoney'=>'updateMyMoney', 'riskCalcMoney'=>'riskCalcMoney', 'isRiskFn'=>'isRiskFn', 'updateTrend'=>'updateTrend', 'watchStatu'=>'watchStatu'];
-    
+    public function mount(){
+        $nowDate = date('Y-m-d H:i');
+        Log::info(date('Y-m-d H:i'));
+        $answer = ModelsAnswer::where('bet_time', $nowDate)->count();
+        if($answer < 1){
+            $this->store();
+        }
+    }
 
     public function sendTime(){
         $beforeTime = date('Y-m-d H:i', strtotime("-4 minute"));
@@ -123,6 +131,52 @@ class Airplane extends Component
     public function watchStatu(){
         $this->statu = GameStatu::where('gamenumber', 23)->first()->statu;
         $this->dispatchBrowserEvent('watchStatu', ['statu' => $this->statu]);
+    }
+    public function store(){
+        $date = date('d');
+        $year = date('Y');
+        $month = date('m');
+        $nowTime = date("H");
+        $twoHour = date("H", strtotime("+2 hour"));
+
+        $date_number = $year."-".$month."-".$date."-".$nowTime."-".$twoHour;
+        // Log::info($date_number);
+        $minute = 0;
+        $hour = intval(date('H'));
+        for($i=1;$i<=120;$i++){
+            $rankingArr = [1,2,3,4,5,6,7,8,9,10];
+            shuffle($rankingArr);
+            $ranking = implode(",",$rankingArr);
+            $minute ++;
+            if($minute >= 60){
+                $hour++;
+                $minute = 0;
+            }
+            if($hour >= 24){
+                $hour = 0;
+            }
+            if($minute < 10){
+                $minuteStr = "0".$minute;
+            }else{
+                $minuteStr = $minute;
+            }
+            if($hour < 10){
+                $hourStr = "0".$hour;
+            }else{
+                $hourStr = $hour;
+            }
+            
+            $number = "SR9359".$year.$month.$date.$hourStr.$minuteStr;
+            // Log::info($number);
+            $bet_time = $year."-".$month."-".$date." ".$hourStr.":".$minuteStr;
+            // Log::info($bet_time);
+            $answer = new ModelsAnswer();
+            $answer->number = $number;
+            $answer->ranking = $ranking;
+            $answer->date_number = $date_number;
+            $answer->bet_time = $bet_time;
+            $answer->save();
+        }
     }
     public function render()
     {
